a0ce27dccb75afb0cfbfa3397d61c3f40685357c
wwq0327 <wwq0327@gmail.com>
2011-12-06 15:47:37 +0800

    main_page

--- urls.py
+++ urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.defaults import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
+from bookmarks.views import *
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django_bookmarks.views.home', name='home'),
@@ -14,4 +16,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+                       (r'^$', main_page),
 )
